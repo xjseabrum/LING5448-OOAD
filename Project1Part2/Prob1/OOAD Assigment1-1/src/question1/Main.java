7b33ac34e2536b7c1a6bcb0082be32b9db73ccbd
WeiTung <samjason515@gmail.com>
2021-09-07 21:40:37 -0600

    Fix issue of file naming and put comments in codes

--- Project1Part2/Prob1/OOAD Assigment1-1/src/question1/Main.java
+++ Project1Part2/Prob1/OOAD Assigment1-1/src/question1/Main.java
@@ -1,21 +1,30 @@
-package first;
+package question1;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class firstClass {
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scanner= new Scanner(System.in);
 		String s;
+		
+		
 		do {
+			//reads a string from the console input
 			s = scanner.nextLine();
+			
+			//change the input string to upper case
 			s=s.toUpperCase();
+			
+			//sort the letters in the string into alphabetic order
 			char charArray[]=s.toCharArray();
 			Arrays.sort(charArray);
+			
+			//print the sorted string to the console
 			System.out.println(charArray);
 		}
+		//program will end if a null string is submitted as input
 		while(!s.isEmpty());
 
 	}
