a0c04c69a1833cc1d33b616134fb4c48509fa98e
xjseabrum <77007903+xjseabrum@users.noreply.github.com>
2021-09-17 22:16:56 -0600

    Updated rng.java
    Updated rng.java to provide two options:
    
    - set and getrandomChance will generate a double between [0, 1)
    - set and getRandomInt will generate an int between [0, maxVal].

--- Project2 (FLGS)/rng.java
+++ Project2 (FLGS)/rng.java
@@ -3,17 +3,31 @@
 // what the code takes in
 // what the code outputs for other objects to use.
 
+// Refined random generator code for integers inspired from:
+// https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+
 import java.lang.Math;
+import java.util.Random;
 
 public class rng{
-    public double rngValue;
+    Random rand = new Random();
+    public double randomChance;
+    public int randomInt;
 
-    public double getRngValue(){
-        return rngValue;
+    public double getRandomChance(){
+        return randomChance;
+    }
+    
+    public int getrandomInt(){
+        return randomInt;
     }
 
-    public void setRngValue(){
-        this.rngValue = Math.random();
+    public void setRandomChance(){
+        this.randomChance = Math.random();
+    }
+    
+    public void setRandomInt(int maxVal){
+        this.randomInt = rand.nextInt(maxVal + 1);
     }
 
 }
