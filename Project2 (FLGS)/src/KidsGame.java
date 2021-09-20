49f17d2aa55f28b753cb7f49451923eb9e6f602b
Jay Seabrum <77007903+xjseabrum@users.noreply.github.com>
2021-09-20 14:48:17 -0600

    Kids SHOULD exist now

---
+++ Project2 (FLGS)/src/KidsGame.java
@@ -0,0 +1,15 @@
+// This is one of Games subclasses.
+// It is a branch in the hierarchy,
+// under which KidsGame, BoardGame, CardGame and FamilyGame
+// inherit their shared attributes, functions, and methods.
+
+// For now, the subclass isn't different from the other subclasses.
+// The only place that they explicitly differ across subclasses is
+// in the value "gameType", by which all objects under this subclass
+// take on this specific value.  This is an example of identity.
+
+public class KidsGame extends Games{
+    public KidsGame(){
+        KidsGame.super.gameType = "Kid's Game";
+    }
+}
