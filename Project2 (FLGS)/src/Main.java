0f7516d84cedeb2c8b9ca0e94dab4af774f5e20e
WeiTung <samjason515@gmail.com>
2021-09-20 14:55:15 -0600

    fix main.java

--- Project2 (FLGS)/src/Main.java
+++ Project2 (FLGS)/src/Main.java
@@ -7,10 +7,14 @@ public class Main {
 
     public static void main(String[] args){
 
+// Instantiate the game objects that are specified in the assignment:
         FamilyGame monopoly = new FamilyGame();
         FamilyGame clue = new FamilyGame();
         FamilyGame life = new FamilyGame();
 
+        KidsGame mousetrap = new KidsGame();
+        KidsGame candyland = new KidsGame();
+        KidsGame connectFour = new KidsGame();
 
         CardGame magic = new CardGame();
         CardGame pokemon = new CardGame();
@@ -20,6 +24,23 @@ public class Main {
         BoardGame risk = new BoardGame();
         BoardGame gloomhaven = new BoardGame();
 
+// Setting the game name manually
+// Surely there is a way to do this more automatically.
+        monopoly.setGameName("Monopoly");
+        clue.setGameName("Clue");
+        life.setGameName("Life");
+
+        mousetrap.setGameName("Mousetrap");
+        candyland.setGameName("Candyland");
+        connectFour.setGameName("Connect Four");
+
+        magic.setGameName("Magic");
+        pokemon.setGameName("Pokemon");
+        netrunner.setGameName("Netrunner");
+
+        catan.setGameName("Catan");
+        risk.setGameName("Risk");
+        gloomhaven.setGameName("Gloomhaven");
         wares.addGame(monopoly);
         wares.addGame(monopoly);
         wares.addGame(clue);
