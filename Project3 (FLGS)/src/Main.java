e89c76fb000f26f4f3fb5da7219cd1f0e2d5f3fa
xjseabrum <77007903+xjseabrum@users.noreply.github.com>
2021-09-30 17:29:19 -0600

    minor changes in main to test deco
    ignorable.

--- Project3 (FLGS)/src/Main.java
+++ Project3 (FLGS)/src/Main.java
@@ -7,24 +7,27 @@ public class Main {
     public static Wares wares = new Wares();
 
     public static void main(String[] args) throws FileNotFoundException {
-        store.simulate(30);
+//        store.simulate(30);
 
         // Testing decorator code.  Ignore.
-//        Games gh = new Gloomhaven();
-//        System.out.println("N: " + gh.getGameName() + " P: $" + gh.getPrice() +
-//                "BL: " + gh.getBoxLength() + "\n");
-//
-////        SpecialAdd gh = new CustomMinis(gh);
-//
-////        gh = (SpecialAdd) new CustomMinis(gh);
+        Games gh = new Gloomhaven();
+        System.out.println("N: " + gh.getGameName() + " P: $" + gh.getPrice() +
+                " BL: " + gh.getBoxLength() + "\n");
+
+//        SpecialAdd gh = new CustomMinis(gh);
+
 //        gh = new CustomMinis(gh);
-//        System.out.println("N: " + ((SpecialAdd) gh).g.getGameName() + " P: $" + gh.getPrice() +
-//                "BL: " + ((SpecialAdd) gh).g.getBoxLength() + "\n");
+        gh = new CustomMinis(gh);
+        System.out.println("N: " + gh.getGameName() + " P: $" + gh.getPrice() +
+                " BL: " + gh.getBoxLength() + "\n");
+
+        gh = new SpecialCards(gh);
+        System.out.println("N: " + gh.getGameName() + " P: $" + gh.getPrice() +
+                " BL: " + gh.getBoxLength() + "\n");
 //
-//        gh = new SpecialCards(gh);
-//        System.out.println("N: " + ((SpecialAdd) gh).g.getGameName() + " P: $" + gh.getPrice() +
-//                "BL: " + ((SpecialAdd) gh).g.getBoxLength() + "\n");
-//
-//        System.out.println(gh.getPrice());
+        System.out.println(gh.getPrice());
+
+        Games gh2 = new Gloomhaven();
+        System.out.println(gh2.getPrice());
     }
 }
