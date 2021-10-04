2480ee2a1385e4c2e9010047f9417013bc1c2f9e
Jay Seabrum <77007903+xjseabrum@users.noreply.github.com>
2021-10-04 10:31:50 -0600

    Manually uploading SpecialCards to the file.
    *sigh*

---
+++ Project3 (FLGS)/src/com/FLGS/Games/SpecialCards.java
@@ -0,0 +1,30 @@
+package com.FLGS.Games;
+
+public class SpecialCards extends SpecialAdd{
+    private int minRoll = 1;
+    private int maxRoll = 6;
+    public String desc;
+    private int numBuy;
+    private double addCost;
+    public double totCost;
+
+    public SpecialCards(Games g) {
+        super(g);
+        this.price = 3.00;
+    }
+
+    public String getDesc(){
+        desc = (" They also bought " + numBuy + "custom mini(s) for $" +
+                String.format("%.2f", addCost) +
+                " for a grand total of $" + totCost + ".");
+        return desc;
+    }
+
+    public double getPrice(){
+        numBuy = super.getRandRoll(minRoll, maxRoll);
+        addCost = numBuy * this.price;
+        totCost = g.getPrice() + addCost;
+        return totCost;
+    }
+
+}
