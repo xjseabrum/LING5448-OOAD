5b8f9a583c8c4da7c30371f46e5a42e916b11ebc
xjseabrum <77007903+xjseabrum@users.noreply.github.com>
2021-10-04 20:10:19 -0600

    Update Main.java

--- Project3 (FLGS)/src/com/FLGS/Main.java
+++ Project3 (FLGS)/src/com/FLGS/Main.java
@@ -16,25 +16,5 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         store.simulate(30);
-
-// Testing decorator code.  This is the syntax to call deco:
-//        Games gh = new Gloomhaven();
-//        Games pk = new Pokemon();
-//        Games mt = new Mousetrap();
-//        Games mo = new Monopoly();
-//        System.out.println(RandomUtils.getRandomDouble() < mo.getExtraBuyChance());
-//        Deco deco = new Deco();
-//
-//        String l = deco.decorate(gh);
-//        System.out.println(l);
-//
-//        String ll = deco.decorate(pk);
-//        System.out.println(ll);
-//
-//        String lll = deco.decorate(mt);
-//        System.out.println(lll);
-//
-//        String llll = deco.decorate(mo);
-//        System.out.println(llll);
     }
 }
