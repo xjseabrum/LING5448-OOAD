f36b7db3c6bf1b9212756456686be2367a433780
xjseabrum <77007903+xjseabrum@users.noreply.github.com>
2021-10-04 19:30:56 -0600

    DECO Class is strategy implementation
    And kinda simple factory too

--- Project3 (FLGS)/src/com/FLGS/Games/Deco.java
+++ Project3 (FLGS)/src/com/FLGS/Games/Deco.java
@@ -1,6 +1,6 @@
 package com.FLGS.Games;
-
 import java.util.Objects;
+import com.FLGS.Store.CashRegister;
 
 public class Deco {
     public String special = "";
@@ -8,36 +8,40 @@ public class Deco {
     public Deco() {
     }
 
-    public String decorate(Games g){
+    public String decorate(Games g, CashRegister r) {
 
-        if (Objects.equals(g.gameName, "Monopoly")){
+        if (Objects.equals(g.gameName, "Monopoly")) {
             Games g2 = g;
             g2 = new SpecialTokenPack(g2);
             this.special = g2.getDesc();
+            r.addCash(g2.getPrice());
             return special;
 
         } else if (Objects.equals(g.gameType, "Card Game")) {
             Games g2 = g;
             g2 = new SpecialCards(g2);
             this.special = g2.getDesc();
+            r.addCash(g2.getPrice());
             return special;
 
         } else if (Objects.equals(g.gameName, "Mousetrap")) {
             Games g2 = g;
             g2 = new SpareParts(g2);
             this.special = g2.getDesc();
+            r.addCash(g2.getPrice());
             return special;
 
         } else if (Objects.equals(g.gameName, "Gloomhaven")) {
             Games g2 = g;
             g2 = new CustomMinis(g2);
             this.special = g2.getDesc();
+            r.addCash(g2.getPrice());
             return special;
 
         } else {
             this.special = "";
+            r.addCash(0);
             return special;
         }
-
     }
 }
