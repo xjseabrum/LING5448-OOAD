072c829ec35d824f3e1895023d99e51c0d61d20e
xjseabrum <77007903+xjseabrum@users.noreply.github.com>
2021-10-04 20:24:23 -0600

    Fixed mixup

--- Project3 (FLGS)/src/com/FLGS/Actions/Count.java
+++ Project3 (FLGS)/src/com/FLGS/Actions/Count.java
@@ -8,7 +8,7 @@ public class Count {
     // We should pass information through object
     // rather than method parameter,
     // see https://stackoverflow.com/questions/4367347/constructor-parameters-vs-method-parameters
-
+    public int cashFairyCounter = 0;
     Cashier cashier;
     public Count(Cashier cashier){
         this.cashier = cashier;
@@ -24,6 +24,7 @@ public class Count {
         double cash = register.getCash();
         double moneyAdded = 1000;
         if(cash < 100){
+            this.cashFairyCounter += 1;
             register.addCash(moneyAdded);
             this.cashier.publish(
                     "The Cash Fairy graciously and magically added $" +
