f777711240cfa2cf53912ea8f35c2b64f660c22d
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2021-10-06 12:42:11 -0600

    Commented Pub, Sub and updated README-FLGS.

--- Project3 (FLGS)/src/com/FLGS/Interfaces/Publisher.java
+++ Project3 (FLGS)/src/com/FLGS/Interfaces/Publisher.java
@@ -2,14 +2,26 @@ package com.FLGS.Interfaces;
 
 import com.FLGS.Store.Employees.Announcer;
 
+// This interface makes a class have publisher functionality.
+// Since a publisher always needs to publish to a subscriber
+// we have implemented a "setSubscriber" method. This will add
+// a reference of a subscriber to the class implementing the interface.
+// Publish will send a String message to the "setSubscriber"
+// getSubscriber is not implemented because there is no reason to call
+// "which subscriber has been published to" because in this sprint - there
+// is only one Announcer. In the future, with more announcer - we can use
+// this function.
+
 public interface Publisher {
 
-    Announcer subscriber = null;
-    // publishes to the subscriber (announcer)
+    // publishes to the subscriber (announcer) who
+    // can then decide what needs to be done
     void publish(String message);
 
     String getSubscriber();
 
+    // adds a reference to a subscriber, through which
+    // Publisher can publish messages.
     void setSubscriber(Announcer announcer);
 
 }
--- Project3 (FLGS)/src/com/FLGS/Interfaces/Subscriber.java
+++ Project3 (FLGS)/src/com/FLGS/Interfaces/Subscriber.java
@@ -2,17 +2,20 @@ package com.FLGS.Interfaces;
 
 import com.FLGS.Store.Employees.Employee;
 
-import java.util.List;
+// This interface just makes a class implement subscriber in the observer pattern.
+// Currently, subscribers don't need references of their publisher to function.
+// We feel that it is more cohesive to not force this relation - so we have left
+// functions adding and removing publishers unimplemented.
+
 
 public interface Subscriber {
 
-    List<Employee> publishers = null;
 
-    void addPublisher(Employee employee);
+    void addPublisher(Employee employee);  // Adds a reference to a publisher.
 
-    void removePublisher(Employee employee);
+    void removePublisher(Employee employee);  // Removes a given publisher from its references
 
-    void removeAllPublishers();
+    void removeAllPublishers(); // Removes all publishers from its references
 
-    void publish(String message);
+    void publish(String message); // Actions to do when a publisher publishes a message to the subscriber.
 }
