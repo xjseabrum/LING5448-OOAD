6deedba843f5ef2c6ca4c430cab395682ab5ba0d
Jay Seabrum <77007903+xjseabrum@users.noreply.github.com>
2021-10-06 18:01:40 -0600

    These two files are the bane of my existence.

---
+++ Project4 (FLGS)/src/com/FLGS/Games/SpecialCards.java
@@ -0,0 +1,40 @@
+package com.FLGS.Games;
+
+// One of the concrete decorator subclasses.
+
+public class SpecialCards extends SpecialAdd{
+    private int minRoll = 1;
+    private int maxRoll = 6;
+    public String desc;
+    private int numBuy;
+    private double addCost;
+    public double totCost;
+
+    public SpecialCards(Games g) {
+        super(g);
+        this.price = 3.00;
+    }
+
+    public String getDesc(){
+        desc = "They also bought " + this.getNumBuy() + " special card(s) for $" +
+                String.format("%.2f", this.getAddCost()) +
+                " for a grand total of $" + String.format("%.2f", this.getPrice())
+                + ".";
+        return desc;
+    }
+
+    public int getNumBuy(){
+        this.numBuy = super.getRandRoll(minRoll, maxRoll);
+        return numBuy;
+    }
+
+    public double getAddCost(){
+        this.addCost = this.numBuy * this.price;
+        return addCost;
+    }
+
+    public double getPrice(){
+        this.totCost = (g.getPrice() + this.addCost);
+        return totCost;
+    }
+}
