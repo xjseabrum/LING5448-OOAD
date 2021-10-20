1fe3bf78e12ba960b06c9e36edfe3e308d844c7c
Jay Seabrum <77007903+xjseabrum@users.noreply.github.com>
2021-10-20 13:21:30 -0600

    Code comments

--- Project4 (FLGS)/src/com/FLGS/Store/StoreVisitors/CardCustomer.java
+++ Project4 (FLGS)/src/com/FLGS/Store/StoreVisitors/CardCustomer.java
@@ -1,3 +1,10 @@
+// One of the customer subclasses.
+// It will be genreated by the factory
+// and will set help kick start setting the probability bias
+// values for the customer type and game type. 
+// See BiasUitls for more information of how the 
+// bias is implemented.
+
 package com.FLGS.Store.StoreVisitors;
 
 public class CardCustomer extends Customer{
