b0433d5acd61ef26e62fcba68ffb65e351a83eeb
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2021-11-11 07:34:27 -0700

    refactoring code template

---
+++ Project6/src/controllers/abstract_controller.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractclassmethod
+
+class AbstractController(ABC) : 
+
+    def __init__(self, state) : 
+        self.state = state
+
+    @abstractclassmethod
+    def execute(self) :
+        raise NotImplementedError('Controller has not defined method execute()')
+
+    @abstractclassmethod
+    def update_state(self) : 
+        raise NotImplementedError('Controller has not defined method update_state()')
+    
+    
+    @abstractclassmethod
+    def transition(self) : 
+        raise NotImplementedError('Controller has not defined method transition()')
